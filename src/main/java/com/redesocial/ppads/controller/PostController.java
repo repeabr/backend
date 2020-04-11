e8ef40543b0d4a49a9a8050f2c67a8e3cdde5498
repeabr <renanusb@gmail.com>
2020-04-10 23:04:51 -0300

    set cross origin annotation

--- src/main/java/com/redesocial/ppads/controller/PostController.java
+++ src/main/java/com/redesocial/ppads/controller/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/redesocial/post")
 public class PostController {
     @Autowired
